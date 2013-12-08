47125688d0b5cfe24d086ca80464cce52c1694ea
Cathleen Gendron <gendron@mit.edu>
2013-12-08 03:10:11 -0500

    GUI functionality (client-side)

--- src/canvas/Canvas.java
+++ src/canvas/Canvas.java
@@ -29,212 +29,226 @@ import javax.swing.SwingUtilities;
  * freehand, with the mouse.
  */
 public class Canvas extends JPanel {
-	// image where the user's drawing is stored
-	private Image drawingBuffer;
-	private final JToggleButton selectErase;
-	private boolean eraseMode;
+    // image where the user's drawing is stored
+    private Image drawingBuffer;
+    private final JToggleButton selectErase;
+    private boolean eraseMode;
+    private Color color = Color.BLACK;
+    private int thickness = 3;
 
-	/**
-	 * Make a canvas.
-	 * 
-	 * @param width
-	 *            width in pixels
-	 * @param height
-	 *            height in pixels
-	 */
-	public Canvas(int width, int height) {
-		this.setPreferredSize(new Dimension(width, height));
+    /**
+     * Make a canvas.
+     * 
+     * @param width
+     *            width in pixels
+     * @param height
+     *            height in pixels
+     */
+    public Canvas(int width, int height) {
+        this.setPreferredSize(new Dimension(width, height));
 
-		eraseMode = false;
-		selectErase = new JToggleButton("Write/Erase");
-		selectErase.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent ie) {
-				if (selectErase.isSelected())
-					eraseMode = true;
-				else
-					eraseMode = false;
-			}
-		});
-		
-		this.add(selectErase);
+        eraseMode = false;
+        selectErase = new JToggleButton("Write/Erase");
+        selectErase.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent ie) {
+                if (selectErase.isSelected())
+                    eraseMode = true;
+                else
+                    eraseMode = false;
+            }
+        });
 
-		addDrawingController();
-		// note: we can't call makeDrawingBuffer here, because it only
-		// works *after* this canvas has been added to a window. Have to
-		// wait until paintComponent() is first called.
-	}
+        this.add(selectErase);
 
-	/**
-	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
-	 */
-	@Override
-	public void paintComponent(Graphics g) {
-		// If this is the first time paintComponent() is being called,
-		// make our drawing buffer.
-		if (drawingBuffer == null) {
-			makeDrawingBuffer();
-		}
+        addDrawingController();
+        // note: we can't call makeDrawingBuffer here, because it only
+        // works *after* this canvas has been added to a window. Have to
+        // wait until paintComponent() is first called.
+    }
 
-		// Copy the drawing buffer to the screen.
-		g.drawImage(drawingBuffer, 0, 0, null);
-	}
+    /**
+     * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        // If this is the first time paintComponent() is being called,
+        // make our drawing buffer.
+        if (drawingBuffer == null) {
+            makeDrawingBuffer();
+        }
 
-	/*
-	 * Make the drawing buffer and draw some starting content for it.
-	 */
-	private void makeDrawingBuffer() {
-		drawingBuffer = createImage(getWidth(), getHeight());
-		fillWithWhite();
-	}
+        // Copy the drawing buffer to the screen.
+        g.drawImage(drawingBuffer, 0, 0, null);
+    }
 
-	/*
-	 * Make the drawing buffer entirely white.
-	 */
-	private void fillWithWhite() {
-		final Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
+    /*
+     * Make the drawing buffer and draw some starting content for it.
+     */
+    private void makeDrawingBuffer() {
+        drawingBuffer = createImage(getWidth(), getHeight());
+        fillWithWhite();
+    }
 
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, getWidth(), getHeight());
+    /*
+     * Make the drawing buffer entirely white.
+     */
+    public void fillWithWhite() {
+        final Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
 
-		// IMPORTANT! every time we draw on the internal drawing buffer, we
-		// have to notify Swing to repaint this component on the screen.
-		this.repaint();
-	}
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, getWidth(), getHeight());
 
-	/*
-	 * Draw a happy smile on the drawing buffer.
-	 */
-	private void drawSmile() {
-		final Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
+        // IMPORTANT! every time we draw on the internal drawing buffer, we
+        // have to notify Swing to repaint this component on the screen.
+        this.repaint();
+    }
 
-		// all positions and sizes below are in pixels
-		final Rectangle smileBox = new Rectangle(20, 20, 100, 100); // x, y,
-																	// width,
-																	// height
-		final Point smileCenter = new Point(smileBox.x + smileBox.width / 2,
-				smileBox.y + smileBox.height / 2);
-		final int smileStrokeWidth = 3;
-		final Dimension eyeSize = new Dimension(9, 9);
-		final Dimension eyeOffset = new Dimension(smileBox.width / 6,
-				smileBox.height / 6);
+    /*
+     * Draw a happy smile on the drawing buffer.
+     */
+    private void drawSmile() {
+        final Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
 
-		g.setColor(Color.BLACK);
-		g.setStroke(new BasicStroke(smileStrokeWidth));
+        // all positions and sizes below are in pixels
+        final Rectangle smileBox = new Rectangle(20, 20, 100, 100); // x, y,
+                                                                    // width,
+                                                                    // height
+        final Point smileCenter = new Point(smileBox.x + smileBox.width / 2,
+                smileBox.y + smileBox.height / 2);
+        final int smileStrokeWidth = 3;
+        final Dimension eyeSize = new Dimension(9, 9);
+        final Dimension eyeOffset = new Dimension(smileBox.width / 6,
+                smileBox.height / 6);
 
-		// draw the smile -- an arc inscribed in smileBox, starting at -30
-		// degrees (southeast)
-		// and covering 120 degrees
-		g.drawArc(smileBox.x, smileBox.y, smileBox.width, smileBox.height, -30,
-				-120);
+        g.setColor(Color.BLACK);
+        g.setStroke(new BasicStroke(smileStrokeWidth));
 
-		// draw some eyes to make it look like a smile rather than an arc
-		for (int side : new int[] { -1, 1 }) {
-			g.fillOval(smileCenter.x + side * eyeOffset.width - eyeSize.width
-					/ 2, smileCenter.y - eyeOffset.height - eyeSize.width / 2,
-					eyeSize.width, eyeSize.height);
-		}
+        // draw the smile -- an arc inscribed in smileBox, starting at -30
+        // degrees (southeast)
+        // and covering 120 degrees
+        g.drawArc(smileBox.x, smileBox.y, smileBox.width, smileBox.height, -30,
+                -120);
 
-		// IMPORTANT! every time we draw on the internal drawing buffer, we
-		// have to notify Swing to repaint this component on the screen.
-		this.repaint();
-	}
+        // draw some eyes to make it look like a smile rather than an arc
+        for (int side : new int[] { -1, 1 }) {
+            g.fillOval(smileCenter.x + side * eyeOffset.width - eyeSize.width
+                    / 2, smileCenter.y - eyeOffset.height - eyeSize.width / 2,
+                    eyeSize.width, eyeSize.height);
+        }
 
-	/*
-	 * Draw a line between two points (x1, y1) and (x2, y2), specified in pixels
-	 * relative to the upper-left corner of the drawing buffer.
-	 */
-	private void drawLineSegment(int x1, int y1, int x2, int y2) {
-		Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
+        // IMPORTANT! every time we draw on the internal drawing buffer, we
+        // have to notify Swing to repaint this component on the screen.
+        this.repaint();
+    }
 
-		if(eraseMode){
-			g.setStroke(new BasicStroke(10));
-			g.setColor(Color.WHITE);
-		} else {
-			g.setStroke(new BasicStroke(3));
-			g.setColor(Color.BLACK);
-		}
-		g.drawLine(x1, y1, x2, y2);
+    public void setColor(Color color) {
+        this.color = color;
+    }
 
-		// IMPORTANT! every time we draw on the internal drawing buffer, we
-		// have to notify Swing to repaint this component on the screen.
-		this.repaint();
-	}
+    public void setErase(boolean erase) {
+        eraseMode = erase;
+    }
 
-	/*
-	 * Add the mouse listener that supports the user's freehand drawing.
-	 */
-	private void addDrawingController() {
-		DrawingController controller = new DrawingController();
-		addMouseListener(controller);
-		addMouseMotionListener(controller);
-	}
+    public void setThickness(int thickness) {
+        this.thickness = thickness;
+    }
 
-	/*
-	 * DrawingController handles the user's freehand drawing.
-	 */
-	private class DrawingController implements MouseListener,
-			MouseMotionListener {
-		// store the coordinates of the last mouse event, so we can
-		// draw a line segment from that last point to the point of the next
-		// mouse event.
-		private int lastX, lastY;
+    /*
+     * Draw a line between two points (x1, y1) and (x2, y2), specified in pixels
+     * relative to the upper-left corner of the drawing buffer.
+     */
+    private void drawLineSegment(int x1, int y1, int x2, int y2) {
+        Graphics2D g = (Graphics2D) drawingBuffer.getGraphics();
 
-		public void DrawingController() {
-			
-		}
-		
-		/*
-		 * When mouse button is pressed down, start drawing.
-		 */
-		public void mousePressed(MouseEvent e) {
-			lastX = e.getX();
-			lastY = e.getY();
-		}
+        if (eraseMode) {
+            g.setStroke(new BasicStroke(10));
+            g.setColor(Color.WHITE);
+        } else {
+            g.setStroke(new BasicStroke(thickness));
+            g.setColor(color);
+        }
+        g.drawLine(x1, y1, x2, y2);
 
-		/*
-		 * When mouse moves while a button is pressed down, draw a line segment.
-		 */
-		public void mouseDragged(MouseEvent e) {
-			int x = e.getX();
-			int y = e.getY();
-			drawLineSegment(lastX, lastY, x, y);
-			lastX = x;
-			lastY = y;
-		}
+        // IMPORTANT! every time we draw on the internal drawing buffer, we
+        // have to notify Swing to repaint this component on the screen.
+        this.repaint();
+    }
 
-		// Ignore all these other mouse events.
-		public void mouseMoved(MouseEvent e) {
-			setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
-		}
+    /*
+     * Add the mouse listener that supports the user's freehand drawing.
+     */
+    private void addDrawingController() {
+        DrawingController controller = new DrawingController();
+        addMouseListener(controller);
+        addMouseMotionListener(controller);
+    }
 
-		public void mouseClicked(MouseEvent e) {
-		}
+    /*
+     * DrawingController handles the user's freehand drawing.
+     */
+    private class DrawingController implements MouseListener,
+            MouseMotionListener {
+        // store the coordinates of the last mouse event, so we can
+        // draw a line segment from that last point to the point of the next
+        // mouse event.
+        private int lastX, lastY;
 
-		public void mouseReleased(MouseEvent e) {
-		}
+        public void DrawingController() {
 
-		public void mouseEntered(MouseEvent e) {
-		}
+        }
 
-		public void mouseExited(MouseEvent e) {
-		}
-	}
+        /*
+         * When mouse button is pressed down, start drawing.
+         */
+        public void mousePressed(MouseEvent e) {
+            lastX = e.getX();
+            lastY = e.getY();
+        }
 
-	/*
-	 * Main program. Make a window containing a Canvas.
-	 */
-	public static void main(String[] args) {
-		// set up the UI (on the event-handling thread)
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				JFrame window = new JFrame("Freehand Canvas");
-				window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				window.setLayout(new BorderLayout());
-				Canvas canvas = new Canvas(800, 600);
-				window.add(canvas, BorderLayout.CENTER);
-				window.pack();
-				window.setVisible(true);
-			}
-		});
-	}
+        /*
+         * When mouse moves while a button is pressed down, draw a line segment.
+         */
+        public void mouseDragged(MouseEvent e) {
+            int x = e.getX();
+            int y = e.getY();
+            drawLineSegment(lastX, lastY, x, y);
+            lastX = x;
+            lastY = y;
+        }
+
+        // Ignore all these other mouse events.
+        public void mouseMoved(MouseEvent e) {
+            setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+        }
+
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        public void mouseExited(MouseEvent e) {
+        }
+    }
+
+    /*
+     * Main program. Make a window containing a Canvas.
+     */
+    public static void main(String[] args) {
+        // set up the UI (on the event-handling thread)
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame window = new JFrame("Freehand Canvas");
+                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                window.setLayout(new BorderLayout());
+                Canvas canvas = new Canvas(800, 600);
+                window.add(canvas, BorderLayout.CENTER);
+                window.pack();
+                window.setVisible(true);
+            }
+        });
+    }
 }
--- src/ui/WhiteboardGUI.java
+++ src/ui/WhiteboardGUI.java
@@ -3,124 +3,264 @@ package ui;
 import canvas.Canvas;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.*;
-import javax.swing.border.MatteBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
-@SuppressWarnings("unused")
-public class WhiteboardGUI extends JFrame {
-    
+public class WhiteboardGUI extends JFrame implements ChangeListener {
+
     private static final long serialVersionUID = 1L;
-    
+
+    /*
+     * GUI-related fields.
+     */
     private final JLabel whiteboardsLabel;
     private final JTable allBoards;
-    private final DefaultTableModel tableModelWhiteboards = new DefaultTableModel(new Object[][] {}, new Object[] {""});
+    private final DefaultTableModel tableModelWhiteboards = new DefaultTableModel(
+            new Object[][] {}, new Object[] { "" }) {
+        @Override
+        public boolean isCellEditable(int row, int col) {
+            return false;
+        }
+    };
     // need to add +/- buttons
-    private final JLabel boardNameLabel;
-    private final JTextField boardName;
     private final JLabel boardEditorsLabel;
     private final JTable boardEditors;
-    private final DefaultTableModel tableModelEditors = new DefaultTableModel(new Object[][] {}, new Object[] {""});
-    private final JTable colorPalette; // needs to be a grid of colors eventually...
+    private final DefaultTableModel tableModelEditors = new DefaultTableModel(
+            new Object[][] {}, new Object[] { "" }) {
+        @Override
+        public boolean isCellEditable(int row, int col) {
+            return false;
+        }
+    };
+    private final JLabel sliderLabel;
+    private final JSlider thicknessSlider;
+    private final JTable colorPalette; // needs to be a grid of colors
+                                       // eventually...
     private final JButton moreColors;
-    private final JButton eraseToggle;
+    private final JToggleButton eraseToggle;
     private final JButton clear;
     private final Canvas canvas;
 
+    /*
+     * Drawing-related fields.
+     */
+    Color[][] colors = {
+            { Color.RED, new Color(255, 128, 0), Color.YELLOW,
+                    new Color(128, 255, 0), Color.GREEN,
+                    new Color(44, 145, 19), Color.BLACK },
+            { Color.CYAN, new Color(0, 128, 255), Color.BLUE,
+                    new Color(128, 0, 255), Color.PINK, Color.MAGENTA,
+                    Color.GRAY } };
+    private int thickness = 3;
+    private Color drawingColor = Color.BLACK;
+    private boolean eraseMode = false;
+
+    @SuppressWarnings("serial")
     public WhiteboardGUI() {
         this.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
-        Insets padding = new Insets(10,10,10,10);
-        
+        Insets padding = new Insets(10, 10, 10, 10);
+
         c.insets = padding;
-        
+
         whiteboardsLabel = new JLabel("Whiteboards");
         c.gridx = 0;
         c.gridy = 0;
         this.add(whiteboardsLabel, c);
-        
-        tableModelWhiteboards.addRow(new Object[] {"01 - trojanHorse"});
-        tableModelWhiteboards.addRow(new Object[] {"02 - twoStrokeEngine"});
-        
+        tableModelWhiteboards.addRow(new Object[] { "01 - trojanHorse" });
+        tableModelWhiteboards.addRow(new Object[] { "02 - twoStrokeEngine" });
+
         allBoards = new JTable(tableModelWhiteboards);
         allBoards.setPreferredSize(new Dimension(0, 200));
+        allBoards.getSelectionModel().addListSelectionListener(
+                new ListSelectionListener() {
+
+                    @Override
+                    public void valueChanged(ListSelectionEvent e) {
+                        System.out.println("I can has dis board?");
+                        // send message to Server
+                    }
+
+                });
+        allBoards.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
         c.gridy = 1;
         this.add(allBoards, c);
-        
-        boardNameLabel = new JLabel("Board Name");
+
         c.fill = GridBagConstraints.NONE;
-        c.gridx = 0;
-        c.gridy = 2;
-        this.add(boardNameLabel, c);
-        
-        boardName = new JTextField(15);
-        c.gridx = 0;
-        c.gridy = 3;
-        this.add(boardName, c);
-        
+
         boardEditorsLabel = new JLabel("Board Editors");
         c.gridx = 0;
-        c.gridy = 4;
+        c.gridy = 2;
         this.add(boardEditorsLabel, c);
-        
-        tableModelEditors.addRow(new Object[] {"apollo"});
-        tableModelEditors.addRow(new Object[] {"zeus"});
-        tableModelEditors.addRow(new Object[] {"athena"});
-        tableModelEditors.addRow(new Object[] {"juno"});
-        
+
+        tableModelEditors.addRow(new Object[] { "apollo" });
+        tableModelEditors.addRow(new Object[] { "zeus" });
+        tableModelEditors.addRow(new Object[] { "athena" });
+        tableModelEditors.addRow(new Object[] { "juno" });
+
         boardEditors = new JTable(tableModelEditors);
+        boardEditors.setRowSelectionAllowed(false);
         c.fill = GridBagConstraints.BOTH;
         c.gridx = 0;
-        c.gridy = 5;
+        c.gridy = 3;
         this.add(boardEditors, c);
-        
-        canvas = new Canvas(800,600);
+
+        sliderLabel = new JLabel("Stroke Thickness");
+        c.fill = GridBagConstraints.NONE;
+        c.gridx = 0;
+        c.gridy = 4;
+        this.add(sliderLabel, c);
+
+        thicknessSlider = new JSlider(1, 10, 3);
+        thicknessSlider.setMajorTickSpacing(9);
+        thicknessSlider.setMinorTickSpacing(1);
+        thicknessSlider.setSnapToTicks(true);
+        thicknessSlider.setPaintTicks(true);
+        thicknessSlider.setPaintLabels(true);
+        thicknessSlider.addChangeListener(this);
+        c.fill = GridBagConstraints.NONE;
+        c.gridx = 0;
+        c.gridy = 5;
+        this.add(thicknessSlider, c);
+
+        canvas = new Canvas(800, 600);
         c.fill = GridBagConstraints.NONE;
         c.gridx = 1;
         c.gridy = 0;
         c.gridheight = 6;
         c.gridwidth = 4;
         this.add(canvas, c);
-        
-        
-        colorPalette = new JTable(2,14);
-        MatteBorder border = new MatteBorder(0,0,0,0, Color.BLACK);
-        colorPalette.setBorder(border);
-        
-        for(int i=0; i<14; i++){
-            colorPalette.getColumnModel().getColumn(i).setPreferredWidth(16);
+
+        DefaultTableModel model = new DefaultTableModel(2, 7) {
+            @Override
+            public boolean isCellEditable(int row, int col) {
+                return false;
+            }
+        };
+        colorPalette = new JTable(model);
+        colorPalette.setGridColor(Color.BLACK);
+        colorPalette.setRowHeight(20);
+        for (int i = 0; i < 7; i++) {
+            colorPalette.getColumnModel().getColumn(i).setPreferredWidth(20);
+            colorPalette.getColumnModel().getColumn(i)
+                    .setCellRenderer(new ColorRenderer());
         }
-        
+
+        colorPalette.getSelectionModel().addListSelectionListener(
+                new ListSelectionListener() {
+
+                    @Override
+                    public void valueChanged(ListSelectionEvent e) {
+                        int row = colorPalette.getSelectedRow();
+                        int col = colorPalette.getSelectedColumn();
+                        drawingColor = colors[row][col];
+                        canvas.setColor(drawingColor);
+                        System.out.println(drawingColor);
+                        // send message to server
+                    }
+
+                });
+
         c.gridx = 1;
         c.gridy = 6;
         c.gridheight = 1;
         c.gridwidth = 1;
         this.add(colorPalette, c);
-        
-        c.insets = new Insets(0,50,0,50);
-        
+
+        c.insets = new Insets(0, 50, 0, 50);
+
         moreColors = new JButton("More Colors");
         c.gridx = 2;
         c.gridy = 6;
         this.add(moreColors, c);
-        
-        eraseToggle = new JButton("Erase");
+
+        eraseToggle = new JToggleButton("Erase");
+        eraseToggle.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent ie) {
+                if (eraseToggle.isSelected())
+                    eraseMode = true;
+                else
+                    eraseMode = false;
+                canvas.setErase(eraseMode);
+            }
+        });
         c.gridx = 3;
         c.gridy = 6;
         this.add(eraseToggle, c);
-        
+
         clear = new JButton("Clear");
+        clear.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                canvas.fillWithWhite();
+                // send message to Server
+            }
+
+        });
         c.gridx = 4;
         c.gridy = 6;
         this.add(clear, c);
-        
+
         this.pack();
-        
+
     }
-    
+
+    /*
+     * Listen to the thickness slider.
+     */
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        JSlider source = (JSlider) e.getSource();
+        if (!source.getValueIsAdjusting()) {
+            thickness = source.getValue();
+            canvas.setThickness(thickness);
+            // send message to Server
+        }
+    }
+
+    /*
+     * Received message adding a Whiteboard to the list.
+     */
+    private void addWhiteboard() {
+
+    }
+
+    /*
+     * Received message adding a User to the list.
+     */
+    private void addUser() {
+
+    }
+
+    public class ColorRenderer extends JLabel implements TableCellRenderer {
+
+        public ColorRenderer() {
+            setOpaque(true);
+        }
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table,
+                Object obj, boolean isSelected, boolean hasFocus, int row,
+                int col) {
+            setBackground(colors[row][col]);
+            return this;
+        }
+
+    }
+
     public static void main(final String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
