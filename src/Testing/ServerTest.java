92708ce67509d305a5a04f805d0e859d98857ed5
Cathleen Gendron <gendron@mit.edu>
2013-12-11 16:44:39 -0500

    Fixed duplicate user test

--- src/Testing/ServerTest.java
+++ src/Testing/ServerTest.java
@@ -40,43 +40,51 @@ public class ServerTest {
             }
         });
         runServer.start();
-        Socket socket = new Socket(InetAddress.getLocalHost().getHostAddress(),
+        
+        // instantiate two clients
+        Socket socket1 = new Socket(InetAddress.getLocalHost().getHostAddress(),
                 50001);
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-                socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        Socket socket2 = new Socket(InetAddress.getLocalHost().getHostAddress(),
+                50001);
+        BufferedReader in1 = new BufferedReader(new InputStreamReader(
+                socket1.getInputStream()));
+        PrintWriter out1 = new PrintWriter(socket1.getOutputStream(), true);
+        BufferedReader in2 = new BufferedReader(new InputStreamReader(
+                socket2.getInputStream()));
+        PrintWriter out2 = new PrintWriter(socket2.getOutputStream(), true);
 
         // test USER_REQ
-        out.println("user_req Fred");
-        String you_are_fred = in.readLine();
-        assertEquals("you_are Fred", you_are_fred);
+        out1.println("user_req Fred");
+        assertEquals("you_are Fred", in1.readLine());
         
         // test duplicate usernames (case-sensitive)
-        out.println("user_req Fred");
-        System.out.println("# Users: " + server.getUserNames().length);
+        out2.println("user_req FRED");
+        assertEquals("you_are user1", in2.readLine());
+        assertEquals(2, server.getUserNames().length);
 
         // test BRD_REQ
-        out.println("board_req new Board");
-        String newBoard = in.readLine();
-        assertEquals("board 1 new Board", newBoard);
+        out1.println("board_req new Board");
+        String newBoard = in1.readLine();
+        assertEquals("board 0 new Board", newBoard);
 
         // test BRD_ALL
-        out.println("board_all");
-        String board_all = in.readLine();
-        assertEquals("board 1 new Board", board_all);
+        out1.println("board_all");
+        String board_all = in1.readLine();
+        assertEquals("board 0 new Board", board_all);
 
-        out.println("board_req anotherOne");
-        in.readLine();
-        out.println("board_all");
-        String[] board_all_array = new String[] { "board 1 new Board",
-                "board 2 anotherOne", };
+        out1.println("board_req anotherOne");
+        in1.readLine();
+        out1.println("board_all");
+        String[] board_all_array = new String[] { "board 0 new Board",
+                "board 1 anotherOne", };
         assertArrayEquals(board_all_array,
-                new String[] { in.readLine(), in.readLine() });
+                new String[] { in1.readLine(), in1.readLine() });
 
         // test BRD_DEL
-        out.println("del 1");
-        out.println("board_all");
-        assertEquals("board 2 anotherOne", in.readLine());
+        out1.println("del 0");
+        in1.readLine();
+        out1.println("board_all");
+        assertEquals("board 1 anotherOne", in1.readLine());
 
     }
 
